cb3f78b29eaf4fcc36cb8d57caf6b879a6257f40
code zero <project.purpose.rollno.29@gmail.com>
2020-09-13 07:23:30 -0700

    Done:collision, commenting Not:Wall Bug

--- main.py
+++ main.py
@@ -5,23 +5,39 @@ import sys
 
 pygame.init()
 
+# Display Initialization
+
 WIDTH = 500
 HEIGHT = 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Rotated Gravity By Siddique Khan')
 
+# Framerate Initialization
+
 FPS = 65 
 clock = pygame.time.Clock()
 
+# Font Initialization
+
 main_font = pygame.font.Font('freesansbold.ttf', 32)
+# lose_font = main_font.render('You Lose')
+
+# Global Variables
 
 thrust = False
 number_of_walls = 4
+player = None
+wall1 = None
+wall2 = None
 wallx = []
 wally = [HEIGHT, HEIGHT+200, HEIGHT+400, HEIGHT+600]
 wally_final = wally.copy()
+run = True
 
 def getWallStatus():
+    """
+    This function handles the (x,y) positions of the wall for the first loop.
+    """
     global number_of_walls, wally, wallx
 
     for i in range(number_of_walls):
@@ -29,6 +45,7 @@ def getWallStatus():
         wallx.append(randomX)
 
 getWallStatus()
+
 
 class Player:
     def __init__(self):
@@ -38,7 +55,8 @@ class Player:
         self.vel_gravity = 10
 
     def draw(self):
-        pygame.draw.rect(WIN, (0, 0, 255), (self.x, self.y, 32, 32))
+        global player
+        player = pygame.draw.rect(WIN, (0, 0, 255), (self.x, self.y, 32, 32))
 
     def gravity(self):
         global thrust
@@ -63,8 +81,14 @@ class Wall:
         self.height1 = 32
     
     def draw(self, x, y):
+        global wall1, wall2
+
         wall1 = pygame.draw.rect(WIN, (255, 0, 0), (x, y, self.width1, self.height1))
         wall2 = pygame.draw.rect(WIN, (255, 0, 0), (x+(self.width1+self.offset), y, self.width1, self.height1))
+
+    def checkCollision(self):
+        if player.colliderect(wall1) or player.colliderect(wall2) or p.x <= 0 or p.x >= WIDTH-32:
+            print('collision detected')
 
     def move(self):
         global wallx, wally, wally_final, number_of_walls
@@ -72,6 +96,9 @@ class Wall:
         for i in range(number_of_walls):
             w.draw(x=wallx[i], y=wally[i])
             wally[i]-=w.vel
+
+            self.checkCollision() # This function is called as this is the loop where walls are generated,so this function will apply for every walls
+
             if wally[i] <= -32:
                 wally[i] = wally_final[i]
             if i == 3:
@@ -82,10 +109,32 @@ class Wall:
 p = Player()
 w = Wall()
     
+
+def loseScreen():
+    global run, FPS, lose_font
+
+    while not run:
+        
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+        
+            if event.type == KEYDOWN:
+                if event.key == K_SPACE:
+                    run = False
+        
+        WIN.blit(lose_font, (HEIGHT/2 - lose_font.get_height()/2, WIDTH/2 - lose_font.get_width()/2))
+        pygame.display.update()
+        clock.tick(FPS)
+
+
     
-run = True
 
 def applyAllFuncs():
+    """
+    This function collects all the function and then call them together in the main game loop.
+    """
     global w, wallx, wally, wally_final
 
     p.draw()
@@ -93,12 +142,8 @@ def applyAllFuncs():
     p.thrust()
     w.move()
 
-def loseScreen():
-    global run
 
-    while not run:
-        pass
-
+# Main Game Loop
 
 def main():
     global run, thrust
